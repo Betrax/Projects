bbfef9ea457ff47261db45857015c76dc83bf675
Joran <joran.verbruggen@Ugent.be>
2021-11-30 14:27:22 +0100

    Stairs for room2 are fixed

--- demo/Room3.java
+++ demo/Room3.java
@@ -2,14 +2,15 @@ import greenfoot.*;
 
 public class Room3 extends World
 {
-
+    prop door = new prop("room2/", 100, 100, 100, 100, 0);
+    
     public Room3()
     {
         super(1200, 700, 1);
-        GreenfootImage background = (new GreenfootImage("banaan.jpg"));
+        GreenfootImage background = (new GreenfootImage("room3.jpg"));
         background.scale(getWidth(), getHeight());
         setBackground(background);
-
+        addObject(door, 100, 100);
     }
 
 
