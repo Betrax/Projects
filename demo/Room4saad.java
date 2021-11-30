50472c909798e757f880e29bd04c8081fa210317
Saad <saad.sami@ugent.be>
2021-11-30 18:37:17 +0100

    Invisible plaatje

--- demo/Room4saad.java
+++ demo/Room4saad.java
@@ -9,19 +9,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Room4saad extends World
 {
     GreenfootImage background = (new GreenfootImage("Verlaten.jpg"));
+    
+    prop door = new prop("Saad/trans1.png", 25, 25, 600, 325, 0);
+    
     prop vraag = new prop("Saad/Vraag.png", 100, 100, 200, 100, 100);
     prop antwoord = new prop("Saad/ANTWOORD A.png", 100, 100, 200, 150, 100);
     prop antwoordb = new prop("Saad/ANTWOORD B.png", 100, 100, 200, 200, 100);
     prop antwoordc = new prop("Saad/ANTWOORD C.png", 100, 100, 200, 250, 100);
+    prop antwoordd = new prop("Saad/ANTWOORD D.png", 100, 100, 200, 300, 100);
     public Room4saad()
     {    
         super(1200, 700, 1); 
         background.scale(getWidth(), getHeight());
         setBackground(background);
         
+        addObject(door, 100, 200);
+    
         addObject(vraag, 100, 200);
         addObject(antwoord, 100, 300);
         addObject(antwoordb, 100, 300);   
         addObject(antwoordc, 100, 300); 
+        addObject(antwoordd, 100, 300); 
+    }
+    
+    public void act()
+    { 
+    if (door.click("tap"))
+        {
+        
+        }
     }
 }
