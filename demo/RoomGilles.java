23af9a1a8ad76c8f0b2d982a4cd946a99066a5a2
Gilles <gilles.vertriest@ugent.be>
2021-12-02 13:57:17 +0100

    binair

--- demo/RoomGilles.java
+++ demo/RoomGilles.java
@@ -1,30 +1,37 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class RoomGilles here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class RoomGilles extends World
 {
     public static World RoomGilles = new RoomGilles();
-    prop desk = new prop("desk.png", 200, 200, getWidth()/2, getHeight()/2 + 100, 0);
+    prop desk = new prop("desk.png", 15, 15, 600, 450, 0);
+    prop nota = new prop("ascii.jpg", 11, 14, 800, 275, 0);
+    prop notaxl = new prop("ascii.jpg", 56, 73, 600, 350, 0);
     
     public RoomGilles()
     {    
-        super(960, 568, 1); 
+        super(1200, 700, 1);
         GreenfootImage background = (new GreenfootImage("basement.png"));
         background.scale(getWidth(), getHeight());
         setBackground(background);
         
-        addObject(desk, getWidth()/2, getHeight()/2 + 100);
+        addObject(desk, 600, 450);
+        addObject(nota, 900, 150);
     }
+    boolean switch1 = true;
     public void act()
     {
-         if (desk.click("tap"))
+        if (desk.click("tap"))
         {
             Greenfoot.setWorld(DesktopGilles.DesktopGilles);
+        }
+        if (nota.click("tap"))
+        {
+            addObject(notaxl, 800, 275);
+            switch1 = false;
+        }
+        if (switch1)
+        {
+            removeObject(notaxl);
         }
     }
     
