28a0efd02d52cfc800818100a5622956efb8b8df
Robin <Robin.vanpuyvelde@gmail.com>
2021-12-05 05:30:51 +0100

    All nighter part 1/3. de rest komt nada'k heb geslapen en heb gedebugged.

--- demo/GOD.java
+++ demo/GOD.java
@@ -13,11 +13,26 @@ public class GOD extends World
     public GOD()
     {
         super(1200, 700, 1);
-        Greenfoot.setWorld(RoomJoran.RoomJoran); // changes the room to the cooresponding room
+                                                       // changes the room to the cooresponding room
                                                        // that is given as an argument. Use the same
                                                        // function with different rooms to switch
                                                        // between them.
-
+                                                    
+                                                       
+        Greenfoot.setWorld(RoomJoran.RoomJoran);                                               
+                                                       
+                                                    /* Copy paste instead of retyping every time :)
+                                                     * RoomRobin.RoomRobin
+                                                     * Room4saad.Room4saad
+                                                     * DesktopJoran.DesktopJoran
+                                                     * DesktopGilles.DesktopGilles
+                                                     * RoomPlaceholder.RoomPlaceholder
+                                                     * Room1Oguz.Room1Oguz
+                                                     * RoomGilles.RoomGilles
+                                                     * RoomJoran.RoomJoran
+                                                     * start_screen.start_screen
+                                                     * 
+                                                     */
     }
     
 }
